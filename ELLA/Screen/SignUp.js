501194c0d1b6e0b22719662d955d944132fa947b
makiroll.mark <delacruzmacky39@gmail.com>
2025-08-29 18:40:21 +0800

    finished the sign in page

---
+++ ELLA/Screen/SignUp.js
@@ -0,0 +1,171 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+
+import { Image } from "expo-image";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+
+export default function SignUp() {
+  const navigation = useNavigation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      {/* Close button (X) */}
+      <TouchableOpacity
+        style={styles.closeButton}
+        onPress={() => navigation.goBack()}
+      >
+        <Ionicons name="close" size={32} color="#fff" />
+      </TouchableOpacity>
+
+      <Text style={styles.title}>Enter your details</Text>
+
+      {/* Email input */}
+      <View style={styles.inputBox}>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          placeholderTextColor="#aaa"
+          value={email}
+          onChangeText={setEmail}
+        />
+      </View>
+
+      {/* Password input */}
+      <View style={styles.inputBox}>
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          placeholderTextColor="#aaa"
+          secureTextEntry={!showPassword}
+          value={password}
+          onChangeText={setPassword}
+        />
+        <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+          <Ionicons
+            name={showPassword ? "eye-off" : "eye"}
+            size={24}
+            color="#555"
+          />
+        </TouchableOpacity>
+      </View>
+
+      {/* Email Sign Up button */}
+      <TouchableOpacity style={styles.buttonEmail}>
+        <Ionicons name="mail" size={32} color="#fff" style={styles.icon} />
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.buttonGoogle}>
+        <Image
+          style={styles.iconGoogle}
+          source={require("../assets/icons/google.png")}
+        />
+        <Text style={[styles.buttonText, styles.googleText]}>Sign Up</Text>
+      </TouchableOpacity>
+      <Image
+        source={require("../assets/animations/jump_owl.gif")}
+        style={styles.gif}
+        contentFit="fill"
+        transition={0}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#60B5FF",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    padding: 20,
+    paddingTop: 150,
+  },
+  closeButton: {
+    position: "absolute",
+    top: 50,
+    left: 20,
+  },
+  gif: {
+    width: 150,
+    height: 150,
+    marginTop: 60,
+  },
+  title: {
+    fontFamily: "PixelifySans",
+    fontSize: 28,
+    color: "#fff",
+    marginBottom: 30,
+    textAlign: "center",
+  },
+  inputBox: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    width: "90%",
+    height: 50,
+    marginBottom: 15,
+    paddingHorizontal: 10,
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    color: "#000",
+    fontFamily: "Poppins",
+  },
+  buttonEmail: {
+    flexDirection: "row",
+    backgroundColor: "#FF9149",
+    width: 250,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 25,
+    marginTop: 20,
+    borderWidth: 1,
+    borderColor: "#fff",
+  },
+  buttonGoogle: {
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    width: 250,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 25,
+    marginTop: 15,
+    borderWidth: 1,
+    borderColor: "#FF9149",
+  },
+  icon: {
+    marginLeft: -20,
+    marginRight: 30,
+  },
+  iconGoogle: {
+    marginLeft: -20,
+    marginRight: 30,
+    width: 32,
+    height: 32,
+  },
+  buttonText: {
+    fontFamily: "Poppins",
+    fontSize: 18,
+    color: "#fff",
+  },
+  googleText: {
+    color: "#000",
+  },
+});
---
+++ ELLA/Screen/StartUp.js
@@ -0,0 +1,161 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { useFonts } from "expo-font";
+import { Ionicons, FontAwesome } from "@expo/vector-icons";
+import { Image } from "expo-image";
+import { useNavigation } from "@react-navigation/native";
+
+export default function StartUp({ navigation }) {
+  const [fontsLoaded] = useFonts({
+    PixelifySans: require("../assets/fonts/PixelifySans-Regular.ttf"),
+    PixelifySansBold: require("../assets/fonts/PixelifySans-Bold.ttf"),
+    Poppins: require("../assets/fonts/Poppins-Regular.ttf"),
+    PoppinsBold: require("../assets/fonts/Poppins-Bold.ttf"),
+  });
+
+  navigation = useNavigation();
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  // ---
+  const handleEmailSignIn = () => {
+    // TODO: Add email sign-in gagawin ni Vik
+    console.log("Email Sign");
+  };
+
+  const handleGoogleSignIn = () => {
+    // TODO: Add Google sign-in
+    console.log("Google Sign");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>ELLA</Text>
+      <Text style={styles.subTitle}>Your English Buddy</Text>
+      <Image
+        source={require("../assets/animations/jump_pink.gif")}
+        style={styles.gif}
+        contentFit="fill"
+        transition={0}
+      />
+      <Text style={styles.welcome}>Welcome!</Text>
+
+      <TouchableOpacity style={styles.buttonEmail} onPress={handleEmailSignIn}>
+        <Ionicons name="mail" style={styles.iconEmail} />
+        <Text style={styles.buttonText}>Sign in with Email</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.buttonGoogle}
+        onPress={handleGoogleSignIn}
+      >
+        <Image
+          style={styles.iconGoogle}
+          source={require("../assets/icons/google.png")}
+        />
+        <Text style={[styles.buttonText, styles.google]}>
+          Sign in with Google
+        </Text>
+      </TouchableOpacity>
+      <View style={styles.signUp}>
+        <Text style={styles.signUpText}>No Account? </Text>
+        <TouchableOpacity>
+          <Text
+            style={[styles.signUpText, styles.signUpTextUnderline]}
+            onPress={() => navigation.navigate("SignUp")}
+          >
+            Sign Up!
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#60B5FF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontFamily: "PixelifySans",
+    fontSize: 96,
+    color: "#fff",
+  },
+  gif: {
+    width: 190,
+    height: 190,
+    margin: 30,
+  },
+  subTitle: {
+    fontFamily: "PixelifySans",
+    fontSize: 24,
+    color: "#fff",
+  },
+  welcome: {
+    fontFamily: "PixelifySans",
+    fontSize: 64,
+    color: "#fff",
+  },
+  buttonEmail: {
+    flexDirection: "row",
+    backgroundColor: "#FF9149",
+    width: 250,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20,
+    marginTop: 20,
+    borderWidth: 1,
+    borderColor: "#fff",
+  },
+  iconEmail: {
+    marginRight: 10,
+    fontSize: 32,
+    color: "#FFF",
+  },
+  buttonGoogle: {
+    flexDirection: "row",
+    backgroundColor: "#FFF",
+    width: 250,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20,
+    marginTop: 20,
+    borderWidth: 1,
+    borderColor: "#FF9149",
+  },
+  iconGoogle: {
+    marginRight: 10,
+    width: 32,
+    height: 32,
+  },
+  buttonText: {
+    fontFamily: "Poppins",
+    fontSize: 14,
+    textAlign: "center",
+    color: "#fff",
+  },
+  google: {
+    color: "#000",
+  },
+  signUp: {
+    flexDirection: "row",
+  },
+  signUpText: {
+    fontFamily: "Poppins",
+    fontSize: 16,
+    color: "#FFF",
+    marginTop: 20,
+  },
+  signUpTextUnderline: {
+    textDecorationLine: "underline",
+  },
+});
